0d211dd716bc6278e118405ffddf975e666fb571
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-09-28 16:56:02 -0400

    initial commit

---
+++ src/constants/data.js
@@ -0,0 +1,30 @@
+const data = {
+  question: {
+    id: 1,
+    body: 'What House of Hogwarts Does Chelsea Belong In?'
+  },
+  answers: [
+    {
+      id: 1,
+      body: 'Gryffindor',
+      correct: false
+    },
+    {
+      id: 2,
+      body: 'Hufflepuff',
+      correct: true
+    },
+    {
+      id: 3,
+      body: 'Ravenclaw',
+      correct: false
+    },
+    {
+      id: 4,
+      body: 'Slytherin',
+      correct: false
+    }
+  ]
+};
+
+export default data;
